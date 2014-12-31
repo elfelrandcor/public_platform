86d443ec0e2666ceed71b7e7bc60f7596de4bdc0
Juriy Panasevich <elfelrandcor@gmail.com>
2014-12-31 10:21:40 +0300

    Контроллер авторизации

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -7,7 +7,14 @@
 
 module.exports = {
 	login: function (request, response) {
-    request.session.authenticated = true;
+    var name = request.param('login');
+    var password = request.param('password');
+    User.findByName(name).exec(function (error, user) {
+      if (password == user.password) {
+        request.session.authenticated = true;
+      }
+    });
+
     return response.redirect("/");
   },
   logout: function (request, response) {
